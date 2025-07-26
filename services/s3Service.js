e5f1875de80de559abada559eeb2b7ae7214825a
TejasJagadale <139218507+TejasJagadale@users.noreply.github.com>
2025-07-26 21:32:34 +0530

    Update s3Service.js

--- services/s3Service.js
+++ services/s3Service.js
@@ -1,4 +1,4 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 // Configure AWS
 AWS.config.update({
@@ -11,26 +11,37 @@ const s3 = new AWS.S3();
 const BUCKET_NAME = process.env.S3_BUCKET_NAME;
 
 exports.getUploadUrl = async (fileType) => {
-  const extension = fileType.split('/')[1]; // Get file extension
-  const key = `uploads/${Date.now()}-${Math.random().toString(36).substring(2, 8)}.${extension}`;
-  
+  const extension = fileType.split("/")[1]; // Get file extension
+  const key = `uploads/${Date.now()}-${Math.random()
+    .toString(36)
+    .substring(2, 8)}.${extension}`;
+
   const params = {
     Bucket: BUCKET_NAME,
     Key: key,
     ContentType: fileType,
     Expires: 60 * 5, // 5 minutes
-    ACL: 'public-read' // Make uploaded files publicly accessible
+    ACL: "public-read" // Make uploaded files publicly accessible
   };
 
   try {
-    const uploadUrl = await s3.getSignedUrlPromise('putObject', params);
-    return { 
-      uploadUrl, 
+    const uploadUrl = await s3.getSignedUrlPromise("putObject", params);
+    return {
+      uploadUrl,
       key,
       publicUrl: `https://${BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`
     };
   } catch (error) {
-    console.error('Error generating S3 URL:', error);
+    console.error("Error generating S3 URL:", error);
     throw error;
   }
+};
+
+exports.deletefile = async (key) => {
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: key
+  };
+
+  await s3.deleteObject(params).promise();
 };
